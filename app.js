42ad7e12d567c734c599e08b63051f9fefbc4a50
rafael <rafaelsanto10s@gmail.com>
2020-11-07 17:07:34 -0300

    busca das letras e paginacao

---
+++ app.js
@@ -0,0 +1,56 @@
+const form  = document.querySelector('#form')
+const searchInput  = document.querySelector('#search')
+const songsContainer  = document.querySelector('#songs-container')
+const prevAndNexContainer  = document.querySelector('#prev-and-next-container')
+
+const apiURL = `https://api.lyrics.ovh`;
+
+
+const getMoreSongs = async url => {
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+    const data = await response.json();
+
+    insertSongsIntoPage(data) 
+}
+
+
+const insertSongsIntoPage = songsInfo => {
+    songsContainer.innerHTML = songsInfo.data.map(({artist, title}) => `
+        <li class="song">
+            <span class="song-artist"><strong>${artist.name}</strong> - ${title}</span>
+            <button class="btn" data-artist="${artist.name}" data-song-title="${title}">Ver letra</button>
+        </li>
+    `).join('');
+
+    if(songsInfo.prev || songsInfo.next) {
+        prevAndNexContainer.innerHTML = `
+            ${songsInfo.prev ? `<button class="btn" onClick="getMoreSongs('${songsInfo.prev}')">Anteriores</button>`: ''}
+            ${songsInfo.next ? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Próximas</button>`: ''}
+        `
+
+        return;
+    }
+
+    prevAndNexContainer.innerHTML = ''
+    
+}
+
+const fetchSongs = async term => {
+    const response = await fetch(`${apiURL}/suggest/${term}`);
+    const data = await response.json();
+
+    insertSongsIntoPage(data)
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const searchTerm = searchInput.value.trim();
+
+    if(!searchTerm){
+        songsContainer.innerHTML = `<li class="warning-message">O campo não pode ficar vázio</li>`;
+        return;
+    }
+
+    fetchSongs(searchTerm);
+})
